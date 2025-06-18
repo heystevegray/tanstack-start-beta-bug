5165894a78a5b01e2a8dfec3e3dd922b9c16239f
Christopher Kiessling <Crsk@mail.com>
2025-06-18 12:47:59 -0400

    migrate to `tanstack/start` version 1.121.21
    https://github.com/TanStack/router/issues/4431#issuecomment-2982310355

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+// app.config.ts
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tsConfigPaths({
+      projects: ["./tsconfig.json"],
+    }),
+    tanstackStart(),
+  ],
+});
